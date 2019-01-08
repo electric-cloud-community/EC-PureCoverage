55d70d221e5cdf7ea32efe44231c77190b66da53
Valentyn Solomko <Valentyn.Solomko@portaone.com>
2019-01-08 15:30:48 +0200

    Adapt for building with Gradle

--- src/main/resources/project/manifest.pl
+++
@@ -1,6 +0,0 @@
-@files = (
-    ['//property[propertyName="purecoverageDriver"]/value', 'purecoverageDriver.pl'],
-    ['//property[propertyName="postp_matchers"]/value', 'postp_matchers.pl'],
-    ['//procedure[procedureName="runPureCoverage"]/propertySheet/property[propertyName="ec_parameterForm"]/value', 'runPureCoverageForm.xml'],
-    ['//property[propertyName="ec_setup"]/value', 'ec_setup.pl'],
-);
